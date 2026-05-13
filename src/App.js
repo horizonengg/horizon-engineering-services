eebb14c50e92d781a2e7c1022a44499e0ffdf355
horizonengg <horizonengg.dev@gmail.com>
2026-05-13 16:41:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import LegoLoader from './components/LegoLoader';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <LegoLoader />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
